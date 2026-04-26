d7bbe4a57406140596e6e6d434e2ac78f6b72bc8
tylarochele4-netizen <tylarochele4@gmail.com>
2026-04-26 23:23:31 +0300

    Add authentication to post routes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/postsController');
+const { protect } = require('../middleware/auth'); // Import the bouncer
 
+// Public routes (Everyone can see)
 router.get('/', postsController.getAllPosts);
-router.post('/', postsController.createPost);
-router.put('/:id', postsController.updatePost);
-router.delete('/:id', postsController.deletePost);
+router.get('/:id', postsController.getPostById);
+
+// Protected routes (Login required)
+router.post('/', protect, postsController.createPost);
+router.put('/:id', protect, postsController.updatePost);
+router.delete('/:id', protect, postsController.deletePost);
 
 module.exports = router;
